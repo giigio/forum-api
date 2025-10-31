8983a1821af196f4771ba82d020d31e06b3ff3ef
Giovanna Leme Amadeu <80579735+giigio@users.noreply.github.com>
2025-10-31 10:41:59 -0300

    refactor: improve choose best answer validation

--- src/domain/forum/application/use-cases/choose-question-best-answer.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -1,4 +1,3 @@
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
